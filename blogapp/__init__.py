530fd2919887b18832d5e45ede47e05b9c96517f
Cozg <nguyenhuucong295@gmail.com>
2026-03-17 23:20:48 +0700

    create repo

---
+++ blogapp/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import cloudinary
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.secret_key = 'JKHJHJK786575ghjghjg78675HJGJHGF^&$%$^*%*&^%&^&*%^&'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/blogdb?charset=utf8mb4"
+# app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["PAGE_SIZE"] = 8
+
+
+mail = Mail(app=app)
+db = SQLAlchemy(app=app)
+login = LoginManager(app=app)
+
+cloudinary.config(cloud_name='dpl8syyb9',
+                  api_key='423338349327346',
+                  api_secret='zfwveRcXlclSOKM7mqSU2j0421c')
