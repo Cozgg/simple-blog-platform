143774251320b49a6ace27f327d20f5f633965a1
Canh Huynh <huynhthecanhpvh@gmail.com>
2026-03-28 10:05:30 +0700

    create feature delete-post-api

---
+++ blogapp/dao.py
@@ -0,0 +1,32 @@
+from blogapp import db
+from blogapp.models import Post, User, UserRole
+
+
+def get_users(id = None):
+    query = User.query
+    if id:
+        return query.get(id)
+    return query.all()
+
+def get_posts(id = None):
+    query = Post.query
+    if id:
+        return query.get(id)
+    return query.all()
+
+def delete_post(post_id, current_user, is_confirmed):
+    p = Post.query.get(post_id)
+    if not p:
+        raise ValueError('Bài viết ko tồn tại')
+
+    if current_user.user_role != UserRole.ADMIN or p.user_id != current_user.id:
+        raise PermissionError('Chỉ admin hoặc tác giả mới được xóa')
+
+    if p.is_pinned:
+        raise ValueError('Bài viết đang ghim ko được xóa')
+
+    if p.comments > 10 and not is_confirmed:
+        raise ValueError('Bài viết có hơn 10 bình luận, cần xác nhận xóa')
+
+    db.session.delete(p)
+    db.session.commit()
--- blogapp/index.py
+++ blogapp/index.py
@@ -1,9 +1,33 @@
-from blogapp import app
-from flask import render_template
+from flask_login import login_required, current_user
+
+from blogapp import app, dao
+from flask import render_template, jsonify, request
+
+from blogapp.models import UserRole
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+@login_required
+def delete_posts(post_id):
+    try:
+        is_firmed = request.args.get('confirmed')
+
+        dao.delete_post(post_id=post_id, current_user=current_user, is_confirmed=is_firmed)
+        return jsonify({
+            'status': 200,
+            "msg": "Xóa thành công"
+        })
+
+    except Exception as e:
+        return jsonify({
+            'status' : 400,
+            'err_msg': str(e)
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
