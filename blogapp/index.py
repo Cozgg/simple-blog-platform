cd96979773dfa45516373c651cc1dd2f95778400
nguyencong-dev <nguyenvancong.ou@gmail.com>
2026-04-06 11:20:24 +0700

    Revert "Feature/add comment api"

--- blogapp/index.py
+++ blogapp/index.py
@@ -5,32 +5,10 @@ from flask import render_template, jsonify, request, redirect
 
 from blogapp.models import UserRole
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/api/comments', methods=['POST'])
-@login_required
-def add_comment():
-    content = request.json.get('content')
-    post_id = request.json.get('post_id')
-
-    try:
-        dao.save_comment(content=content, post_id=post_id, user_id=current_user.id)
-        return jsonify({
-            "status": 201,
-            "msg": "Đã đăng tải thành công bình luận",
-        })
-    except PermissionError as e:
-        return jsonify({
-            "status": 400,
-            "err_msg": str(e)
-        })
-    except Exception as ex:
-        return jsonify({
-            "status": 500,
-            "err_msg": "Lỗi hệ thống không xác định"
-        })
 
 
 @app.route('/login')
